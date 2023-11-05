ed6d601321b8d67560e7ebb944901bfe932fa083
jaskaransingh0606 <sardarshinesingh12@gmail.com>
2023-11-05 17:00:44 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Hp
+#
+# Created:     05-11-2023
+# Copyright:   (c) Hp 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print("hello world")
+a=4;
+a=6;
+
+print(a);
+
+name=input("enter your name");
+print("hello "+name);
---
+++ typeConversion.py
@@ -0,0 +1,11 @@
+old_age=input("Enter your age: ")
+new_age=int(old_age)+50
+print(new_age)
+
+
+# we can convert one type of data to another type of data
+
+# float()
+# int()
+# str()
+# bool()
