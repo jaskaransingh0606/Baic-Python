e0dbdf0a3a9ca3ff46855086c6670e90ea837514
jaskaransingh0606 <sardarshinesingh12@gmail.com>
2023-11-28 00:37:35 +0530

    classes

---
+++ classes.py
@@ -0,0 +1,20 @@
+class Human:
+    def __init__(self,name,occupation):
+        self.name = name
+        self.occupation = occupation
+        
+    def do_work(self):
+            if self.occupation == "tennis player":
+                print(self.name,"plays tennis")
+            elif self.occupation == "actor":
+                print(self.name,"shoots film")
+                
+    def speaks(self):
+            print(self.name,"says how are you?")
+            
+            
+tom = Human("Tom Cruise","actor")
+tom.do_work()   
+tom.speaks()
+                
+                
---
+++ exceptionHandling.py
@@ -0,0 +1,4 @@
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+y = int(x)/int(y)
+print("The result is: ", y)
